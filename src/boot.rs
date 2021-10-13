ef6e3ce9cf06a80e012fa38072567ecaa548a233
Dodo <kasper199914@gmail.com>
2021-10-13 21:42:54 +0200

    setup:

---
+++ src/boot.rs
@@ -0,0 +1,29 @@
+#[repr(packed)]
+pub struct Multiboot {
+    magic: i32,
+    flags: i32,
+    checksum: i32
+}
+
+const ALIGN: i32 = 1 << 0;
+const MEMINFO: i32 = 1 << 1;
+const MAGIC: i32 = 0x1BADB002;
+const FLAGS: i32 = ALIGN | MEMINFO;
+
+#[no_mangle]
+#[link_section = ".multiboot"]
+pub static multiboot: Multiboot = Multiboot {
+    magic: MAGIC,
+    flags: FLAGS,
+    checksum: -(MAGIC + FLAGS),
+};
+
+/// 4 pages, PAGE_SIZE aligned.
+#[repr(align(4096))]
+pub struct AlignedStack([u8; 4096 * 4]);
+
+/// The stack we start on.
+///
+/// The first thing we do is to make $esp point to it.
+#[link_section = ".bss"]
+pub static mut STACK: AlignedStack = AlignedStack([0; 4096 * 4]);
